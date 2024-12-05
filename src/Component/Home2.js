8b4b4ebaa16e0e0b1f9717c8c8a75adf01a0b608
Bharat Karle <karlebn19.comp@coeptech.ac.in>
2024-12-05 18:04:08 +0530

    Initial commit

---
+++ src/Component/Home2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "../Css/Home2.css"
+import image1 from "../Img/Bg.jpg"
+export class Home2 extends Component {
+  render() {
+    return (
+      <div className="img">
+        
+      </div>
+    )
+  }
+}
+
+export default Home2
